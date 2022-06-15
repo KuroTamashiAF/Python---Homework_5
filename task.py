86b7a7c8c929de0451168e2594bf4b50aca5b814
Alex <chugun777akk@mail.ru>
2022-06-15 08:56:00 +0700

    Task 2 is done

--- task.py
+++ task.py
@@ -20,7 +20,65 @@ print(total_list)
 
 # 2. Вы когда-нибудь играли в игру "Крестики-нолики"? Попробуйте создать её,
 # причем чтобы сыграть в нее можно было в одиночку.
+'''
+board = [i for i in range(1, 10)]
+wins_combination = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
+                    (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
 
+
+def draw_board():
+    print('-'*13)
+    for i in range(3):
+        print('|', board[0+i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
+    print('-'*13)
+
+
+def input_from_user(player_input):
+    while True:
+        value = input("В какую ячейку поставить: " + player_input + " ?")
+        if not (value in '123456789'):
+            print('Неверный ввод. Повторите')
+            continue
+        value = int(value)
+        if str(board[value-1]) in 'XO':
+            print('Эта клетка уже занята')
+            continue
+        board[value-1] = player_input
+        break
+
+
+def wins_check():
+    for each in wins_combination:
+        if(board[each[0]-1]) == (board[each[1]-1]) == (board[each[2]-1]):
+            return board[each[0]-1]
+    else:
+        return False
+
+
+def main():
+
+    counter = 0
+    while True:
+        draw_board()
+        if counter % 2 == 0:
+            input_from_user('X')
+        else:
+            input_from_user('O')
+        if counter > 3:
+            winner = wins_check()
+            if winner:
+                draw_board()
+                print(winner, 'Победил')
+                break
+        counter += 1
+        if counter > 8:
+            draw_board()
+            print('Ничья')
+            break
+
+
+main()
+'''
 # 3. Вот вам текст:«Ну, вышел я, короче, из подъезда. В общем, короче говоря,
 # шел я, кажется, в магазин. Ну,эээ, в общем, было лето, кажется. Как бы тепло.
 # Солнечно, короче. Иду я, иду, в общем, по улице, а тут, короче, яма.
@@ -30,7 +88,6 @@ print(total_list)
 # Вышел из подъезда, короче. Лето на дворе, ясен пень. Птицы поют, короче, солнечно.
 # В общем, в магазин мне надо. Что-то явно не так, короче. «Рекурсия», - подумал я.
 # Ээээ...короче, в общем, пошел другой дорогой и не упал в эту… ээээ… яму. Хлеба купил».
-
 
 # Отфильтруйте его, чтобы этот текст можно было нормально прочесть.
 # Предусмотрите вариант, что мусорные слова могли быть написаны без использования запятых.
